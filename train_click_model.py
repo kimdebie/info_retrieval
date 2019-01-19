2be5d414e29b61c7c06035d1ea1357a2856fdac8
kimdebie <kimdebie@outlook.com>
2019-01-19 18:04:59 +0100

    running pbm parameters now

--- train_click_model.py
+++ train_click_model.py
@@ -2,10 +2,7 @@ import pandas as pd
 import csv
 import random
 
-# data = pd.read_csv("YandexRelPredChallenge.txt", sep="\t", header=None, names=list(range(0, 16)))
 data = "YandexRelPredChallenge.txt"
-
-
 
 def learn_model_parameters(data):
 
@@ -24,12 +21,12 @@ def learn_model_parameters(data):
                     pass
                 query = row
                 query.append([])
-                for i in range(5, 8):
+                for i in range(5, 11):
                     query[15].append(query[i])
                     query[i] = False
 
             else:
-                for i in range(0, 3):
+                for i in range(0, 6):
                     if row[3] == query[15][i]:
                         query[i+5] = True
 
@@ -54,44 +51,64 @@ def learn_model_parameters(data):
     # alpha (attractiveness) is defined per unique query-document pair
     alphas = {}
     for index in qd_pairs["QueryDocPair"].unique():
-        alphas[index] = 0.5
+        alphas[index] = 0.2
     print("Total alphas to be trained: " + str(len(alphas)))
 
     # gamma (examination) is defined per rank
-    gammas = [0.2, 0.2, 0.2]
+    gammas = [0.5, 0.5, 0.5, 0.5, 0.5, 0.5]
 
+    # As we are primarily interested in gammas (we will discard alphas)
+    # convergence is determined on the basis of gammas.
+    delta_gamma = 1
+    i = 0
 
-    # Expectation step
-    for i in range(0, 100):
-        print(i)
+    while delta_gamma > 0.3:
+        print("Round " + str(i))
+        i+= 1
 
-        # update rule for alpha parameters
-        # print("Update alphas")
-        for index in qd_pairs["QueryDocPair"].unique(): # update one alpha for each QD pair
+        print("Update alphas")
+
+        # update one alpha for each QD pair
+        for i, index in enumerate(qd_pairs["QueryDocPair"].unique()):
+
+            if i%1000 == 0:
+                print(str(i) + " parameters trained in this round")
+
+            # take sessions and alpha for this particular Q-D pair
             sess_qd = qd_pairs[qd_pairs["QueryDocPair"] == index]
             old_alpha = alphas[index]
 
+            # following the update formula for each session
             sum_of_clicks_alpha = 0
-            for i, session in sess_qd.iterrows(): # for each session of a QD pair
+            for i, session in sess_qd.iterrows():
 
+                # getting the relevant parameters for the formula
                 click_u = session["Clicked"]
                 old_gamma = gammas[session["Rank"]]
 
+                # The Formula
                 sum_of_clicks_alpha += click_u + (1-click_u) * \
                  ((1-old_gamma)*old_alpha / 1-old_gamma*old_alpha)
 
+            # final update to this alpha
             alphas[index] = 1/sess_qd.shape[0] * sum_of_clicks_alpha
 
 
-        # update rule for examination
         print("Update gammas")
+
+        # track change in deltas to determine convergence
+        delta_gammas = 0
+
+        # update one alpha per rank
         for rank, gamma in enumerate(gammas):
 
+            # take all sessions and the gamma for this particular rank
             sess_rank = qd_pairs[qd_pairs["Rank"] == rank]
             old_gamma = gammas[rank]
 
             sum_of_clicks_gamma = 0
             for i, session in sess_rank.iterrows():
+
                 click_u = session["Clicked"]
                 old_alpha = alphas[session["QueryDocPair"]]
 
@@ -99,7 +116,15 @@ def learn_model_parameters(data):
                 (old_gamma*(1-old_alpha) / 1-old_gamma*old_alpha)
 
             gammas[rank] = 1/len(sess_rank) * sum_of_clicks_gamma
-        print(gammas)
+            delta_gammas += abs(gammas[rank] - old_gamma)
+
+        print("Change in gammas: " + str(delta_gammas))
+
+    alphas_df = pd.DataFrame(alphas, orient='index')
+    alphas_df.to_csv('trained_alphas.csv')
+
+    gammas_df = pd.DataFrame({'gammas': gammas})
+    gammas_df.to_csv('trained_gammas.csv')
 
     return alphas, gammas
 
