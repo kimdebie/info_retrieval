6cf2240709ce75371032098ee4150e625c295984
kimdebie <kimdebie@outlook.com>
2019-01-19 16:20:26 +0100

    pbm parameters

--- train_click_model.py
+++ train_click_model.py
@@ -9,6 +9,8 @@ data = "YandexRelPredChallenge.txt"
 
 def learn_model_parameters(data):
 
+    # Cleaning data
+
     completed_queries = []
 
     with open(data) as f:
@@ -22,55 +24,84 @@ def learn_model_parameters(data):
                     pass
                 query = row
                 query.append([])
-                for i in range(5, 15):
+                for i in range(5, 8):
                     query[15].append(query[i])
-                    query[i] = False#[query[i], False]
+                    query[i] = False
 
             else:
-                for i in range(0, 10):
+                for i in range(0, 3):
                     if row[3] == query[15][i]:
                         query[i+5] = True
 
-
     headers = ['SessionID', 'TimePassed', 'TypeOfAction', 'QueryID', 'RegionID', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'ResultIDs']
-
     data = pd.DataFrame(completed_queries, columns=headers)
-    print(data.head())
+    #print(data.head())
+
+    query_doc_pairs = [] #[queryID-docID, rank, clicked]
+    for index, query in data.iterrows():
+        for i, doc in enumerate(query['ResultIDs']):
+            row = [str(query['QueryID']) + '-' + str(doc), i, query[i]]
+            query_doc_pairs.append(row)
+
+    headers2 = ["QueryDocPair", "Rank", "Clicked"]
+    qd_pairs = pd.DataFrame(query_doc_pairs, columns=headers2)
 
 
+    ## EM algorithm for finding optimal alpha and gamma ##
 
+    # Set parameters to initial values
 
-    # EM algorithm for finding optimal alpha and gamma
+    # alpha (attractiveness) is defined per unique query-document pair
+    alphas = {}
+    for index in qd_pairs["QueryDocPair"].unique():
+        alphas[index] = 0.5
+    print("Total alphas to be trained: " + str(len(alphas)))
 
-    # alpha (attractiveness) is defined per query-document pair
     # gamma (examination) is defined per rank
+    gammas = [0.2, 0.2, 0.2]
 
-    # Set parameters to random initial values
-    alpha = random.uniform(0, 1)
-    gamma = random.uniform(0, 1)
 
-    # update rule for alpha parameter
-    # search_sess_query_doc: needs all search sessions with a particular query ID and doc ID, plus whether this doc was clicked or not
-    #
-    # sum_of_clicks_alpha = 0
-    # for session in search_sess_query_doc:
-    #     click_u =
-    #     sum_of_clicks_alpha += click_u + (1-click_u) *
-    #      ((1-old_gamma)*old_alpha / 1-old_gamma*old_alpha)
-    #
-    # alpha = 1/len(search_sess_query_doc) * sum_of_clicks_alpha
-    #
-    # # update rule for examination
-    # # search_sess_all: needs all search sessions, plus whether each doc was clicked or not
-    # sum_of_clicks_gamma = 0
-    # for session in search_sessions_all:
-    #     click_u =
-    #     sum_of_clicks_gamma += click_u + (1-click_u) *
-    #     (old_gamma*(1-old_alpha) / 1-old_gamma*old_alpha)
-    #
-    # gamma = 1/len(search_sessions) * sum_of_clicks_gamma
+    # Expectation step
+    for i in range(0, 100):
+        print(i)
 
-    return alpha, gamma
+        # update rule for alpha parameters
+        # print("Update alphas")
+        for index in qd_pairs["QueryDocPair"].unique(): # update one alpha for each QD pair
+            sess_qd = qd_pairs[qd_pairs["QueryDocPair"] == index]
+            old_alpha = alphas[index]
+
+            sum_of_clicks_alpha = 0
+            for i, session in sess_qd.iterrows(): # for each session of a QD pair
+
+                click_u = session["Clicked"]
+                old_gamma = gammas[session["Rank"]]
+
+                sum_of_clicks_alpha += click_u + (1-click_u) * \
+                 ((1-old_gamma)*old_alpha / 1-old_gamma*old_alpha)
+
+            alphas[index] = 1/sess_qd.shape[0] * sum_of_clicks_alpha
+
+
+        # update rule for examination
+        print("Update gammas")
+        for rank, gamma in enumerate(gammas):
+
+            sess_rank = qd_pairs[qd_pairs["Rank"] == rank]
+            old_gamma = gammas[rank]
+
+            sum_of_clicks_gamma = 0
+            for i, session in sess_rank.iterrows():
+                click_u = session["Clicked"]
+                old_alpha = alphas[session["QueryDocPair"]]
+
+                sum_of_clicks_gamma += click_u + (1-click_u) * \
+                (old_gamma*(1-old_alpha) / 1-old_gamma*old_alpha)
+
+            gammas[rank] = 1/len(sess_rank) * sum_of_clicks_gamma
+        print(gammas)
+
+    return alphas, gammas
 
 learn_model_parameters(data)
 
